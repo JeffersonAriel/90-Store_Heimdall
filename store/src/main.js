bd32d27d6df9282157357ba6bc3b794545590c69
JeffersonAriel <tekoqz@gmail.com>
2026-07-05 19:33:58 -0300

    \Deploy: Corrige requisicoes AJAX da API e caminhos de imagens em subpastas"

--- store/src/main.js
+++ store/src/main.js
@@ -3,7 +3,18 @@ import { createPinia } from 'pinia'
 import { createHead } from '@vueuse/head'
 import router from './router'
 import App from './App.vue'
+import axios from 'axios'
 import './assets/css/main.css'
+
+// Detecta dinamicamente a subpasta do cPanel (ex: /~jeff2892)
+const basePath = window.location.pathname.replace(/\/$/, '')
+
+axios.interceptors.request.use((config) => {
+  if (config.url && config.url.startsWith('/api/')) {
+    config.url = `${basePath}${config.url}`
+  }
+  return config
+})
 
 const app = createApp(App)
 const pinia = createPinia()
